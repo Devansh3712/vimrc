9e37d3542b4b46d7fa23bd6bf3dd4e23185c48b1
Devansh3712 <devanshamity@gmail.com>
2020-09-27 12:21:53 +0530

    vim config

---
+++ templates/skeleton.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <string>
+
+typedef long long int ll;
+typedef long int l;
+typedef long double ld;
+
+const double pi = acos(-1);
+const long long mod = 1e9 + 7;
+
+#define loop(a,n) for(int i=a;i<n;i++)
+#define fe(arr) for(int x : arr)
+#define max(x,y) (x>y)?x:y
+#define min(x,y) (x<y)?x:y
+#define sqr(x) x*x
+
+//<scanf>
+#define sf scanf
+
+#define tc() int t; scanf("%d",&t); while(t--)
+
+#define i1(x) int x; scanf("%d",&x)
+#define i2(x,y) int x,y; scanf("%d %d",&x,&y)
+#define i3(x,y,z) int x,y,z; scanf("%d %d %d",&x,&y,&z)
+
+#define ll1(x) long long int x; scanf("%lld",&x)
+#define ll2(x,y) long long int x,y; scanf("%lld %lld",&x,&y)
+#define ll3(x,y,z) long long int x,y,z; scanf("%lld %lld %lld",&x,&y,&z)
+//</scanf>
+
+//<printf>
+#define pf printf
+
+#define pi1(x) printf("%d\n",x)
+#define pi2(x,y) printf("%d %d\n",x,y)
+#define pi3(x,y,z) printf("%d %d %d\n",x,y,z)
+
+#define pll1(x) printf("%lld\n",x)
+#define pll2(x,y) printf("%lld %lld\n",x,y)
+#define pll3(x,y,z) printf("%lld %lld %lld\n",x,y,z)
+//</printf>
+
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+	solve();
+	return 0;
+}
+
+
